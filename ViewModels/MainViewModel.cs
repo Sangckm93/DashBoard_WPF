4cbb4824abd2b7b11a1db12a61b9653dbdb92e5a
Super Man <sangckm93@hotmail.com>
2024-02-03 00:30:02 +0700

    Finish Dashboard

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DashBoard_WPF.Models;
 using DashBoard_WPF.Repositories;
+using FontAwesome.Sharp;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace DashBoard_WPF.ViewModels
 {
@@ -15,8 +17,12 @@ namespace DashBoard_WPF.ViewModels
     {
         // Fields
         private UserAccountModel _currentUserAccount;
+        private ViewModelBase _currenChildView;
+        private string _caption;
+        private IconChar _icon;
         private IUserRepository userRepository;
 
+        // Properties
         public UserAccountModel CurrentUserAccount 
         { 
             get => _currentUserAccount; 
@@ -27,11 +33,63 @@ namespace DashBoard_WPF.ViewModels
             } 
         }
 
+        public ViewModelBase CurrenChildView 
+        { 
+            get => _currenChildView;
+            set
+            {
+                _currenChildView = value;
+                OnPropertyChanged(nameof (CurrenChildView));
+            }
+        }
+        public string Caption 
+        { 
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon 
+        { 
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        // ...-> Commands
+        public ICommand ShowHomeViewCommand { get; } 
+        public ICommand ShowCustomerViewCommand { get; } 
         public MainViewModel()
         {
             userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+            // Initialize Command
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+            // Default View
+            ExecuteShowHomeViewCommand(null);
+
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrenChildView = new CustomerViewModel();
+            Caption = "Customers";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrenChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrentUserData()
@@ -40,7 +98,7 @@ namespace DashBoard_WPF.ViewModels
 
             if (user != null) {
                 CurrentUserAccount.UserName = user.Username;
-                CurrentUserAccount.DisplayName = $"Welcome {user.Name} {user.LastName} ;)";
+                CurrentUserAccount.DisplayName = $"{user.Name} {user.LastName}";
                 CurrentUserAccount.ProfilePicture = null;
             }
             else
